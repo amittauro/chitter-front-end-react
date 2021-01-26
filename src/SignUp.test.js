cdbeff22fe24fbb057c22c387e2e46966d8a7277
Amit Tauro <tauroamit@gmail.com>
2021-01-26 17:18:29 +0000

    test that sign up form can be filled in

--- src/SignUp.test.js
+++ src/SignUp.test.js
@@ -1,4 +1,5 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SignUp from './SignUp';
 
 test('renders form for signing up', () => {
@@ -7,4 +8,14 @@ test('renders form for signing up', () => {
   const password = screen.getByLabelText('Password:');
   expect(username).toBeInTheDocument();
   expect(password).toBeInTheDocument();
+})
+
+test('can fill in the form', () => {
+  render(<SignUp />);
+  const username = screen.getByLabelText('Username:');
+  const password = screen.getByLabelText('Password:');
+  userEvent.type(username, 'user')
+  userEvent.type(password, 'password')
+  expect(username).toHaveValue('user')
+  expect(password).toHaveValue('password')
 })

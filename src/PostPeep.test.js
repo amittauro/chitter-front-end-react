da8d8aada8a854710fd4f6a36b33a8f587f4365b
Amit Tauro <tauroamit@gmail.com>
2021-01-27 10:52:42 +0000

    test that user can fill in post peep form

--- src/PostPeep.test.js
+++ src/PostPeep.test.js
@@ -7,3 +7,10 @@ test('renders form for posting peep', () => {
   const peep = screen.getByLabelText('Peep:');
   expect(peep).toBeInTheDocument();
 })
+
+test('can fill in the form', () => {
+  render(<PostPeep />);
+  const peep = screen.getByLabelText('Peep:');
+  userEvent.type(peep, 'my first peep')
+  expect(peep).toHaveValue('my first peep')
+})
